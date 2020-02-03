9e5aece0a3e5b7b4911ed45f2e5520c4363b0296
Jiuk Choi <jiukchoi@cjuartintelligence.com>
2020-02-03 16:01:28 -0600

    several updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
+import { render } from '@testing-library/react';
+
+
+const customers = [
+{
+  'id': 1,
+  'image':'https://placeimg.com/64/64/1',
+  'name': 'John Doe',
+  'birthday': '96/12/12',
+  'gender': 'male',
+  'job': 'college student'
+},
+{
+  'id': 2,
+  'image':'https://placeimg.com/64/64/2',
+  'name': 'Jonathan Smith',
+  'birthday': '94/06/12',
+  'gender': 'male',
+  'job': 'Software Developer'
+},
+{
+  'id': 3,
+  'image':'https://placeimg.com/64/64/3',
+  'name': 'Jiuk Choi',
+  'birthday': '85/05/12',
+  'gender': 'male',
+  'job': 'Freelancer'
+}
+]
 
 function App() {
   return (
-    <div className='gray-background'>
-      <img src={logo} lat='logo' />
-      <h2>Let's develope management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            )
+          }
+        )
+      }
     </div>
   );
 }
-
 export default App;
