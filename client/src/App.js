f08d4667e5ad980b4ebbe11c236df762a5402971
Jiuk Choi <jiukchoi@cjuartintelligence.com>
2020-02-05 02:10:44 -0600

    another server updates

--- client/src/App.js
+++ client/src/App.js
@@ -23,35 +23,25 @@ const styles = theme => ({
   }
 })
 
-
-const customers = [
-{
-  'id': 1,
-  'image':'https://placeimg.com/64/64/1',
-  'name': 'John Doe',
-  'birthday': '96/12/12',
-  'gender': 'male',
-  'job': 'college student'
-},
-{
-  'id': 2,
-  'image':'https://placeimg.com/64/64/2',
-  'name': 'Jonathan Smith',
-  'birthday': '94/06/12',
-  'gender': 'male',
-  'job': 'Software Developer'
-},
-{
-  'id': 3,
-  'image':'https://placeimg.com/64/64/3',
-  'name': 'Jiuk Choi',
-  'birthday': '85/05/12',
-  'gender': 'male',
-  'job': 'Freelancer'
-}
-]
-
 class App extends React.Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err=>console.log(err));
+
+  }
+
+  callApi = async() =>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const { classes } = this.props;
     return (
@@ -68,7 +58,9 @@ class App extends React.Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> ) } ) }
+            {this.state.customers ? this.state.customers.map(c => { 
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} /> );
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req,res)=> {
-    res.send({message: 'Hello user~!'});
+app.get('/api/customers', (req,res)=> {
+    res.send(
+        [
+            {
+              'id': 1,
+              'image':'https://placeimg.com/64/64/1',
+              'name': 'John Doe',
+              'birthday': '96/12/12',
+              'gender': 'male',
+              'job': 'college student'
+            },
+            {
+              'id': 2,
+              'image':'https://placeimg.com/64/64/2',
+              'name': 'Jonathan Smith',
+              'birthday': '94/06/12',
+              'gender': 'male',
+              'job': 'Software Developer'
+            },
+            {
+              'id': 3,
+              'image':'https://placeimg.com/64/64/3',
+              'name': 'Jiuk Choi',
+              'birthday': '85/05/12',
+              'gender': 'male',
+              'job': 'Freelancer'
+            }
+        ]);
 });
 
 app.listen(port,() => console.log(`Listening on port ${port}`));
